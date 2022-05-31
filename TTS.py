536436c79f68c04848dfef7fd87e2005d5bbcb70
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-05-31 21:32:19 +0200

    refactor

--- TTS.py
+++ TTS.py
@@ -1,22 +1,31 @@
 import time, pyttsx3
 import multiprocessing
 
-def __speak__(text, voiceId):
+import global_variables
+import constants
+
+def __speak__(text, voiceId, volume):
+	if global_variables.voice_assistant:
+		global_variables.voice_assistant.app.icon.set_icon(constants.TRAY_ICON_SPEAKING, constants.TRAY_TOOLTIP + ": " + text)
 	engine = pyttsx3.init()
+	engine.setProperty('volume', volume)
 	engine.setProperty('voice', voiceId)
 	engine.say(text)
 	engine.runAndWait()
+	#if global_variables.voice_assistant:
+	#	global_variables.voice_assistant.app.icon.set_icon(constants.TRAY_ICON_IDLE, constants.TRAY_TOOLTIP + ": Bereit")
 		
 class Voice:
 
 	def __init__(self):
 		self.process = None
 		self.voiceId = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_DE-DE_HEDDA_11.0"
+		self.volume = 0.5
 		
 	def say(self, text):
 		if self.process:
 			self.stop()
-		p = multiprocessing.Process(target=__speak__, args=(text, self.voiceId))
+		p = multiprocessing.Process(target=__speak__, args=(text, self.voiceId, self.volume))
 		p.start()
 		self.process = p
 		
@@ -27,19 +36,25 @@ class Voice:
 		if self.process:
 			self.process.terminate()
 			
+	# Überprüfe, ob derzeit gesprochen wird
 	def is_busy(self):
 		if self.process:
-			return self.process.is_alive()			
+			return self.process.is_alive()
+			
+	# Setze die Lautstärke mit der gesprochen wird
+	def set_volume(self, volume=0.5):
+		self.volume = volume
 		
 	def get_voice_keys_by_language(self, language=''):
 		result = []
 		engine = pyttsx3.init()
 		voices = engine.getProperty('voices')
 		
+		# Wir hängen ein "-" an die Sprache in Großschrift an, damit sie in der ID gefunden wird
 		lang_search_str = language.upper()+"-"
 		
 		for voice in voices:
-			# explode voice path
+			# Die ID einer Sprache ist beispielsweise:
 			# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_DE-DE_HEDDA_11.0
 			if language == '':
 				result.append(voice.id)
--- intents/functions/gettime/intent_gettime.py
+++ intents/functions/gettime/intent_gettime.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from loguru import logger
 import pytz
 import global_variables
 import os
