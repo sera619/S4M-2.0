d576487f5fe5fe272ed28bd0f21ecc295f3ddc1f
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-06-06 14:07:03 +0200

    create more helper functions to handle users

--- ui_utils.py
+++ ui_utils.py
@@ -1,24 +1,77 @@
-
+from loguru import logger
 import json, os, sys, time, datetime
 from tinydb import TinyDB, Query
+from tinydb.operations import set as tset
 from usermgmt import UserMgmt
+
 
 class UIutils:
     def __init__(self):
         self.db = TinyDB('users.json')
         self.speaker_table = self.db.table('speakers')
-        self.user_to_edit = None
+
+    def edit_name(self,user,name):
+        speaker = Query()
+        edit_user = self.speaker_table.get(speaker.name == str(user))
+        edit_user['name'] = name
+        self.speaker_table.update(edit_user,speaker.name == str(user))
+        logger.debug('\nnew username set : ', name)
+
+    def edit_number(self, user, number):
+        speaker = Query()
+        edit_user = self.speaker_table.get(speaker.name == str(user))
+        edit_user['phone'] = number
+        self.speaker_table.update(edit_user,speaker.name == str(user))
+        logger.debug('\nnew number set at '+user+ ' : '+str(number))
+
+    def edit_lang(self, user, lang):
+        speaker = Query()
+        edit_user = self.speaker_table.get(speaker.name == str(user))
+        edit_user['language'] = lang
+        self.speaker_table.update(edit_user,speaker.name == str(user))
+
+    def edit_permissions(self, user, permissions = False):
+        speaker = Query()
+        edit_user = self.speaker_table.get(speaker.name == str(user))
+        if permissions:
+            edit_user['intents'] = "[*]"
+            self.speaker_table.update(edit_user,speaker.name == str(user))
+            logger.debug('\nUser '+user+' is not a guest anymore')
+
+        else:
+            edit_user['intents'] = "['animalsounds','gettime']"
+            self.speaker_table.update(edit_user,speaker.name == str(user))
+            logger.debug('\nUser '+user+' is now a guest')
+
+    def edit_gender(self, user, gender):
+        speaker = Query()
+        edit_user =self.speaker_table.get(speaker.name ==str(user))
+        edit_user['gender'] = gender
+        logger.debug('\nnew gender set at '+ user + ' : ' + str(gender))
+
+    def edit_mail(self, user, mail):
+        speaker = Query()
+        edit_user = self.speaker_table.get(speaker.name == str(user))
+        edit_user['mail'] = mail
+        logger.debug('\nnew mail set at '+user+ ' : '+str(mail))
 
     def is_new_user(self):
         if len(self.speaker_table) == 2:
             return True
         else:
             return False
+
     def get_user_number(self, name):
         speaker = Query()
         user = self.speaker_table.search(speaker.name == str(name))
         userphone =  user[0]['phone']
         return str(userphone)
+
+    def get_user_mail(self, name):
+        speaker = Query()
+        user = self.speaker_table.search(speaker.name == str(name))
+        usermail =  user[0]['mail']
+        return str(usermail)
 
     def get_gender(self,name):
         speaker = Query()
@@ -63,8 +116,8 @@ class UIutils:
     def get_table(self):
         return self.speaker_table
 
-    def create_user(self, name, gender, phone, voice, permissions,lang):
-        self.speaker_table.insert({'name': name, 'gender':gender, 'voice': voice,'phone':phone,'intents': permissions,'language':lang})
+    def create_user(self, name, gender, phone, voice, permissions, lang, mail):
+        self.speaker_table.insert({'name': name, 'gender':gender, 'voice': voice,'phone':phone,'intents': permissions,'language':lang,'mail':mail})
 
 
     def get_user_list(self):
@@ -74,48 +127,12 @@ class UIutils:
             users.append(user['name'])
         if len(users) == 2:
             users = []
-        print(users)
+        logger.debug("\n"+users)
         return users
-
-
-
-class Styles:
-    def applyButtonStyle():
-    
-        was =    """    QPushButton{
-        border-radius:20px;
-        border-color: rgb(0, 255, 255);
-        }
-        QPushButton:hover{
-            border-color: rgb(0, 255, 255);
-            color: black;
-            background-color: rgb(170, 255, 255);
-        }
-        QPushButton:pressed {
-            border-color: rgb(0, 255, 255);
-            color: black;	
-            background-color: rgb(0, 255, 255);
-        }
-        """
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def main():
     utils = UIutils()
-
     choice = int(input("\n1. Create new user\n2. Get user list\n3. Exit\n"))
     if choice == 1:
         print('not now')
@@ -126,30 +143,6 @@ def main():
     else:
         print('this option is not available')
         return main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__=='__main__':
     try:
